38227b99072bcde75ba52a309989ffc5156ff10f
u.dukenbay <urziya.dukenbay@gmail.com>
2021-01-12 20:02:31 +0600

    add modalState

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,13 +1,10 @@
-const forms =() => {
-    const form = document.querySelectorAll('form'),
-          inputs = document.querySelectorAll('input')
-          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+import checkNumInputs from './checkNumInputs';
 
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        });
-    });
+const forms =(state) => {
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input');
+
+    checkNumInputs('input[name="user_phone"]');
 
     const message = {
         loading: 'Загрузка...',
@@ -40,6 +37,11 @@ const forms =() => {
             item.appendChild(statusMessage);
 
             const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
 
             postData('assets/server.php', formData)
                 .then(res => {
@@ -52,6 +54,13 @@ const forms =() => {
                     setTimeout(() => {
                         statusMessage.remove();
                     }, 5000);
+                    // *** Close Modal Window
+                    // document.querySelector('.popup_form').style.display = 'none';
+                    // document.body.style.overflow = '';
+                    // *** modalState почистить
+                    //state = {};
+                    // *** 
+                    // проверить все ли формы заполнены
                 });
 
         });
